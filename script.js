e5389d4a67a9ebdffdd002b104ed2fee8ee51e60
mikethebro <xenophon.of.athens@gmail.com>
2017-04-10 12:08:12 -0600

    seperating files

---
+++ script.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBs6Gxmg8j_wicTwOSZ-CToaBSAzo7X-kA",
+  authDomain: "kneefixerpro.firebaseapp.com",
+  databaseURL: "https://kneefixerpro.firebaseio.com",
+  projectId: "kneefixerpro",
+  storageBucket: "kneefixerpro.appspot.com",
+  messagingSenderId: "342257651978"
+};
+firebase.initializeApp(config);
