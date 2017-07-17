c4e93b617a4f24504c47584de4ddb3182cde944f
Mike Ward <xenophon.of.athens@gmail.com>
2017-07-17 12:30:26 -0600

    adding whitenoise to wsgi

--- kneefixerpro/wsgi.py
+++ kneefixerpro/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kneefixerpro.settings")
+
 
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kneefixerpro.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
